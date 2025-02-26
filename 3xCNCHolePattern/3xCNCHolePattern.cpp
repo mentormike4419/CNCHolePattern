0ef4fdd5348ff0c02bb7f020d85ec858a543a298
mentormike4419 <mentormike@hallack.com>
2025-02-25 17:41:55 -0800

    update

--- 3xCNCHolePattern/3xCNCHolePattern.cpp
+++ 3xCNCHolePattern/3xCNCHolePattern.cpp
@@ -1,26 +1,43 @@
 // CNCHolePattern.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // MJH:10_07_2024:manual cnc drill pattern output tap file for use on omio cnc
 // MJH:10_08_2024:update to create gcode tap files automatically from 1 to 50 
+// MJH: 2/25/2025: working great. spindle to 12000 from 9000 / Added tube length in inches
 // test3
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
-
+//truncate trailing zeros in tube length during convert from double to string
+std::string truncateToOneDecimalPlace(double value) {
+    std::stringstream ss;
+    ss << std::fixed << std::setprecision(1) << value;
+    return ss.str();
+}
 
 int main() {
-    // set speed and feed
-    int spindle{ 9000 };
+    // declare feed & speed
+    int spindle{ 12000 };
     int feed{ 450 };
-    int holes{ 50 }; // max number of holes
+    int holes{ 5 }; // max number of holes
+
+    // get tubelength
+    double tubeLength{ 1 }; //start tube length at 1 inch
+    std::string tubeLengthString{};
 
     // loop through from 1 to holes
     int Hcount{ 1 }; // loop Hcount from 1 to holes
     while (Hcount <= holes) {
+        // cumulate tube length using number of holes plus 0.5in
+        // increment (0.5 in) tubeLength at end of loop
+        tubeLengthString = truncateToOneDecimalPlace(tubeLength);
+
         //create output filename
-        std::string fname = ("3x" + std::to_string(Hcount) + "_Alum_HolePattern.tap");
+        std::string fname = ("3x" + std::to_string(Hcount) + " (" + tubeLengthString + " in)" + " AlumHolePattern.tap");
         std::ofstream outf{ fname };
+
         // If we couldn't open the output file stream for writing
         if (!outf)
         {
@@ -28,14 +45,14 @@ int main() {
             std::cerr << "Uh oh, Sample.txt could not be opened for writing!\n";
             return 1;
         }
+
         // print out manually the startup gcode 
-        outf << "(ALUM 1X HOLE PATTERN)\n";
-        outf << "(T7  D=4.978 CR=0. TAPER=135DEG - ZMIN=-5.206 - DRILL)\n";
+        outf << "(3x" + std::to_string(Hcount) + " [" + tubeLengthString + "in]" + " AlumHolePattern.tap)\n";
         outf << "G90 G94 G91.1 G40 G49 G17\n";
         outf << "G21\n";
         outf << "(OPERATION COMMENT 9DRILL ALUM HOLE PATTERN)\n";
         outf << "M5\n";
-        outf << "T7 M6\n";
+        outf << "T1 M6\n";
         outf << "S" << spindle << " M3\n";
         outf << "G54\n";
         outf << "(4419 POST EDIT TO FORCE Z AXIS MOVE BEFORE G0)\n";
@@ -64,5 +81,6 @@ int main() {
         outf << "Z15\n";
         outf << "M30\n";
         ++Hcount;
+        tubeLength = tubeLength + 0.5;
     }
 }
--- MK4nTubeCNCHolePattern/MK4nTubeCNCHolePattern.cpp
+++ MK4nTubeCNCHolePattern/MK4nTubeCNCHolePattern.cpp
@@ -1,43 +1,30 @@
 // CNCHolePattern.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // MJH:10_07_2024:manual cnc drill pattern output tap file for use on omio cnc
 // MJH:10_08_2024:update to create gcode tap files automatically from 1 to 50 
-// MJH: 2/25/2025: working great. spindle to 12000 from 9000 / Added tube length in inches
 // test3
 
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <sstream>
-#include <iomanip>
 
-//truncate trailing zeros in tube length during convert from double to string
-std::string truncateToOneDecimalPlace(double value) {
-    std::stringstream ss;
-    ss << std::fixed << std::setprecision(1) << value;
-    return ss.str();
-}
+
 
 int main() {
-    // declare feed & speed
-    int spindle{ 12000 };
+    // set speed and feed
+    int spindle{ 9000 };
     int feed{ 450 };
-    int holes{ 70 }; // max number of holes
-
-    // get tubelength
-    double tubeLength{ 1 }; //start tube length at 1 inch
-    std::string tubeLengthString{};
+    int holes{ 3 }; // max number of holes
 
     // loop through from 1 to holes
-    int Hcount{ 1 }; // loop Hcount from 1 to holes
-    while (Hcount <= holes) {
-        // cumulate tube length using number of holes plus 0.5in
-        // increment (0.5 in) tubeLength at end of loop
-        tubeLengthString = truncateToOneDecimalPlace(tubeLength);
-        
+    // int Hcount{ 1 }; // loop Hcount from 1 to holes
+    // while (Hcount <= holes) {
         //create output filename
-        std::string fname = ("1x" + std::to_string(Hcount) + " (" + tubeLengthString + " in)" + " AlumHolePattern.tap");
-        std::ofstream outf{ fname };
+        
+        // convert while loop to one time for the mk4n end holes
+        // std::string fname = ("MK4n" + std::to_string(Hcount) + "_Alum_HolePattern.tap");
+        std::string fname = ("MK4n_Alum_HolePattern.tap");
 
+    std::ofstream outf{ fname };
         // If we couldn't open the output file stream for writing
         if (!outf)
         {
@@ -45,15 +32,14 @@ int main() {
             std::cerr << "Uh oh, Sample.txt could not be opened for writing!\n";
             return 1;
         }
-
         // print out manually the startup gcode 
-        outf << "(1x" + std::to_string(Hcount) + " [" + tubeLengthString + "in]" + " AlumHolePattern.tap)\n";
-        // outf << "(T1  D=4.978 CR=0. TAPER=135DEG - ZMIN=-5.206 - DRILL)\n";
+        outf << "(MK4n HOLE PATTERN)\n";
+        outf << "(T7  D=4.978 CR=0. TAPER=135DEG - ZMIN=-5.206 - DRILL)\n";
         outf << "G90 G94 G91.1 G40 G49 G17\n";
         outf << "G21\n";
         outf << "(OPERATION COMMENT 9DRILL ALUM HOLE PATTERN)\n";
         outf << "M5\n";
-        outf << "T1 M6\n";
+        outf << "T7 M6\n";
         outf << "S" << spindle << " M3\n";
         outf << "G54\n";
         outf << "(4419 POST EDIT TO FORCE Z AXIS MOVE BEFORE G0)\n";
@@ -66,10 +52,10 @@ int main() {
         outf << "G98 G81 X0. Y0. Z-5.0 R5.08 F" << feed << ".\n";
 
         //loop while for each x hole move 12.7mm in Y
-        double yDist = { 12.7 };
+        double yDist = { 25.4 };
         int Ycount{ 1 };
         // count -1 bc hole 1 is at Y0.0
-        while (Ycount <= (Hcount - 1)) {
+        while (Ycount <= (holes - 1)) {
             outf << "Y" << (yDist * Ycount) << "\n";
             ++Ycount;
         }
@@ -77,10 +63,6 @@ int main() {
         outf << "G80\n";
         outf << "Z15\n";
         outf << "M30\n";
-        
-        // Increment Loop Vars
-        ++Hcount;
-        tubeLength = tubeLength + 0.5;
-    }
-    std::cout << "Complete";
+        ++holes;
+    // }
 }
